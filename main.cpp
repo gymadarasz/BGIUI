bb9f4e5f692d3368581bc86b9b54a42a7dbb44e2
gymadarasz <gyula.madarasz@gmail.com>
2018-06-14 20:59:33 +0100

    first: init eclipse project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jun 14, 2018
+ *      Author: Gyula
+ */
+
+#include <graphics.h>
+#include <stdio.h>
+
+int main() {
+	initwindow(10,10,"hello");
+	printf("Hello World!1");
+	getch();
+	return 0;
+}
+
+
